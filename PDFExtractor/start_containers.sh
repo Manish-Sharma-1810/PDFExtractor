7f196e4a7267430e329c6bb420ea4c15d56730bf
Manish-Sharma-1810 <manishkumar.sharma0103@gmail.com>
2024-05-11 00:22:18 +0530

    Added VPC and Load Balancer

--- PDFExtractor/start_containers.sh
+++ PDFExtractor/start_containers.sh
@@ -26,4 +26,4 @@ done
 docker volume create portainer_data
 
 # Run portainer container
-docker container run -d --name portainer -p 8080:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker container run -d --name portainer -p 9000:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
